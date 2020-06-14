10756feffdd46eb7d5efd75caf244d6811a69d8c
barhouum7 <bb.med2000@gmail.com>
2020-06-14 01:15:10 +0100

    Created routes and send the first response | Export my router

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,13 @@
 /* Creating the index Route */
 const express = require('express')
 const router = express.Router() /* Get the router portion of that Express Varible */
+
+// With this router Varible now we can create our Routes
+router.get('/', (req, res) => {
+    res.send('Hello World !') // We just sending a response
+})
+
+/* We should export our router 
+to allow our application 
+to get information from this index file */
+module.exports = router
